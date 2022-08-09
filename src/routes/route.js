091904f95e65b6bcbd9a66ac49a94b2f67ec8794
Pravin <pravinpatekar1999@gmail.com>
2022-08-09 09:45:11 +0530

    assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,82 @@
 const express = require('express');
-const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
-
-    res.send('My second ever api!')
+router.get('/movies' , function(req ,res){
+    const array = ['vikram' , 'war' , 'golmal' ,' luck']
+    res.send(array);
+    console.log(array);
+});
+router.get('/movies/:indexNumber' , function(req ,res){
+    const array = ['vikram' , 'war' , 'golmal' ,' luck']
+    const reqPara= req.params;
+    const index = reqPara.indexNumber ;
+    if( index < array.length ){
+        return res.send(array[index]);
+    }
+    else{
+        return res.send("Plz Enter Valid Index");
+    }
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
+router.get("/films" , function (req , res){
+    const obj = [{
+        "id" : 1,
+        "name" : "Harry Potter and the Philosopher's Stone"
+    },
+    {
+        "id" : 2 ,
+        "name" : "Final Destination 5"
+    },
+    {
+        "id" : 3 ,
+        "name" : "It Chapter Two"
+    },
+    {
+        "id" : 4 ,
+        "name" : "Insidious: Chapter 4"
+    },
+    {
+        "id" : 5 ,
+        "name" : "The Conjuring: The Devil Made Me Do It"
+    }];
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
+    res.send(obj);
+});
 
-    let requestParams = req.params
+router.get("/films/:filmId" , function (req , res){
+    const array = [{
+        "id" : 1,
+        "name" : "Harry Potter and the Philosopher's Stone"
+    },
+    {
+        "id" : 2 ,
+        "name" : "Final Destination 5"
+    },
+    {
+        "id" : 3 ,
+        "name" : "It Chapter Two"
+    },
+    {
+        "id" : 4 ,
+        "name" : "Insidious: Chapter 4"
+    },
+    {
+        "id" : 5 ,
+        "name" : "The Conjuring: The Devil Made Me Do It"
+    }];
 
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
-
+    const reqParas = req.params;
+    const filmId = reqParas.filmId ;
+    const result = function (){
+        for (let i=0 ; i< array.length ;i++){
+            const obj = array[i];
+            if(obj['id'] == filmId){
+                return res.send(obj)
+            }
+        }
+        return res.send("No Such Film Exist With This Id")
+    }
+    result();
+    //res.send(array);
+});
 module.exports = router;
