c8dbcbdef030d1e92c1dcc9adb00bae298c99d14
Pravin <pravinpatekar1999@gmail.com>
2022-08-31 21:38:06 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Saurav-functionup:IcZ9Vkt89tyUiYBq@cluster0.0phxx.mongodb.net/Sauravm89", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
---
+++ src/middlewares/middleware.js
@@ -0,0 +1,11 @@
+const validate = function (req,res,next){
+    const appUser = req.headers['isfreeappuser']
+    
+   if(!appUser){
+       res.send("Plz enter isFreeAppUser property in header")
+    }
+
+    req['appUser'] = Boolean(appUser)
+    next()
+}
+module.exports.validate = validate
